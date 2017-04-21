4a21418add8e69dd86757bdcaae459bcf283503f
Neil <neil.renaud@gmail.com>
2017-04-21 13:50:55 +0100

    Add Bridge Handler

---
+++ addons/binding/org.openhab.binding.antiferencematrix/src/main/java/org/openhab/binding/antiferencematrix/handler/AntiferenceMatrixBridgeHandler.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2014-2016 by the respective copyright holders.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.antiferencematrix.handler;
+
+
+
+public class AntiferenceMatrixBridgeHandler extends BaseBridgeHandler {
+
+    public void connect(){
+    }
+
+
+    public void changeSource(){
+    }
+
+    public void changePower(){
+    }
+
+}
